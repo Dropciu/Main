fe764e997afed8cf91e489517dcbed247fa8a41c
Kacperrek69 <96318629+Kacperrek69@users.noreply.github.com>
2022-04-17 12:01:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-$("html, body").animate({ scrollTop: 0, scrollLeft: 0 }, "slow");
+window.scroll({
+    top: 0, 
+    left: 0, 
+    behavior: 'smooth'
+});
 
 
 
@@ -17,6 +21,7 @@ if(window.location.protocol == "http"){
 }
 
 
+
 if(window.innerWidth < 1200){
 
     document.querySelector(':root').style.setProperty('--zmp', (-(1200 - window.innerWidth / 1)).toString() + "px" );
@@ -32,6 +37,55 @@ if(window.innerWidth < 1200){
     document.querySelector(':root').style.setProperty('--zmp6', (-(1200 - window.innerWidth / 0.55)).toString() + "px" );
 
 }
+
+
+
+
+
+
+if(!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) && window.innerWidth > 1200){
+    document.getElementById("nav").style.visibility = "visible";
+
+
+    document.getElementById("nav1").addEventListener('click',
+    function(event) {
+        window.scroll({
+            top: parseFloat(getComputedStyle(document.getElementById("p2")).top) + parseFloat(getComputedStyle(document.getElementById("p2")).margin) / 2, 
+            left: 0, 
+            behavior: 'smooth'
+        });
+
+    }, { once: true });
+
+
+
+    document.getElementById("nav2").addEventListener('click',
+    function(event) {
+        window.scroll({
+            top: parseFloat(getComputedStyle(document.getElementById("p3")).top) + parseFloat(getComputedStyle(document.getElementById("p3")).margin) / 2,
+            left: 0, 
+            behavior: 'smooth'
+        });
+
+    }, { once: true });
+
+
+
+    window.addEventListener("scroll", function scroll(){
+        if(window.pageYOffset > window.innerWidth / 10){
+            window.removeEventListener('scroll', scroll);
+
+            document.getElementById("nav").remove();
+        }
+    },);
+
+}
+else{
+    document.getElementById("nav").remove();
+}
+
+
+
 
 
 
@@ -67,61 +121,6 @@ window.onload = (event) => {
 
     }
 
-
-
-
-
-    if(!((/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) || window.innerWidth < 1200)){
-        document.getElementById("nav").innerHTML = "<p id='nav1'>⇣ Do repozytoriów ⇣</p><p id='nav2'>⇣ Do osiągnięć ⇣</p>";
-
-
-        document.getElementById("nav1").addEventListener('click',
-        function(event) {
-
-            window.scroll({
-
-                top: window.innerHeight, 
-
-                left: 0, 
-
-                behavior: 'smooth'
-
-            });
-
-            document.getElementById("nav").remove();
-
-        }, false);
-
-
-
-        document.getElementById("nav2").addEventListener('click',
-        function(event) {
-
-            window.scroll({
-
-                top: 2 * window.innerHeight, 
-
-                left: 0, 
-
-                behavior: 'smooth'
-
-            });
-
-            document.getElementById("nav").remove();
-
-        }, false);
-
-
-
-        window.addEventListener("scroll", function(){
-            if(window.pageYOffset > window.innerWidth / 10){
-                document.getElementById("nav").remove();
-            }
-        },);
-
-    }
-
-    
 
 };
 
